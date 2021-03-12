c76214129473d1d5813757663df76a47626d9f77
Taishiro Inokuchi <tai0721abc@gmail.com>
2021-03-12 23:12:28 +0900

    :sparkles:  ミスで全データで一番スタンプ押した人を集計してた。一応コミットしとく

--- aggregator/kind_user_aggregator.rb
+++ aggregator/kind_user_aggregator.rb
@@ -1,5 +1,3 @@
-require 'json'
-
 class KindUserAggregator
   attr_accessor :channel_names
 
@@ -9,7 +7,13 @@ class KindUserAggregator
 
   # 実装してください
   def exec
-    
+    stamp_count = channel_names.map do |channel|
+      data = load(channel)
+      reactions = data.dig('messages').flat_map { |message| message.dig('reactions', 0, 'users') }.compact
+      reactions.group_by(&:itself).map { |key, value| [key, value.count] }.to_h
+    end
+    stamp_count = {}.merge(*stamp_count).sort_by { |key, value| value }.to_h
+    stamp_count.map { |key, value| { user_id: key, reaction_count: value } }.reverse
   end
 
   def load(channel_name)
